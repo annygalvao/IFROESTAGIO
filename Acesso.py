83695f74d340137536a2faea263277d6f8409148
Anny <annymaria8865@gmail.com>
2024-12-17 11:54:59 -0400

    Ultimas alterações em Acesso.py
    Corrigi erros em try-except. Adicionei: while True e break, finally e comando raise para definir possíveis exceções.

--- Acesso.py
+++ Acesso.py
@@ -12,14 +12,26 @@ def cadastro(lista):
         print('''\nEscreva suas informações NESSA ORDEM:
 Nome, Senha, Área, Período, Dias Disponíveis, anoEscolar, idade, contato:''')
 
+
         nome = input('Nome: ').strip()
         senha = input('Senha: ').strip()
         area = input('Área: ').strip()
         periodo = input('Período: ').strip()
         diasdisponiveis = input('Dias Disponíveis: ').strip()
         anoEscolar = input('Ano escolar: ').strip()
-        idade = input('Idade: ').strip()
-        contato = input('Contato: ').strip()
+        while True:
+            try:
+                idade = int(input('Idade: '))
+                break 
+            except ValueError:
+                    print ("Ocorreu um erro! Insira um número inteiro válido.")
+        while True:
+            try:
+                contato = int(input('Contato: '))
+                break
+            except ValueError:
+                print ("Ocorreu um erro! Insira um número inteiro válido.")
+            
 
         aluno_obj = Aluno(nome, senha, area, periodo, diasdisponiveis,anoEscolar,idade,contato)
         print(f"\nCadastro realizado: {aluno_obj.get_info()}")
@@ -27,19 +39,38 @@ Nome, Senha, Área, Período, Dias Disponíveis, anoEscolar, idade, contato:''')
 
     elif '2' in escolha or 'EMPRESA' in escolha.upper():
         print('''\nEscreva suas informações NESSA ORDEM:
-Nome, Senha, Área, Período, Salário, Carga horária, CNPJ e as info do seu Supervisor:''')
+    Nome, Senha, Área, Período, Salário, Carga horária, CNPJ e as info do seu Supervisor:''')
 
         nome = input('Nome: ').strip()
         senha = input('Senha: ').strip()
         area = input('Área: ').strip()
         periodo = input('Período: ').strip()
-        salario = input('Salário: ').strip()
-        cargaHoraria = input('Carga hóraria: ').strip()
+        
+
+        while True:   
+            try:
+                salario = int(input('Salário oferecido: '))
+                if salario < 300:
+                    raise Exception ("O salário não pode ser inferior a 300 reais")
+                break 
+            except:
+                print("O salário não pode ser inferior a 300 reais")
+                
+        while True:
+            try:
+                cargaHoraria = int(input('Carga hóraria: ')) 
+                break
+            except ValueError: 
+                print ("Insira dígitos numéricos válidos")
+            finally: 
+                print("Para concluir o cadastro")
+         
+
         cnpj = input('CNPJ: ').strip()
         Snome = input('Supervisor nome: ').strip()
         Scontato = input('Supervisor contato: ').strip()
         Scargo = input('Supervisor carg: ').strip()
-
+            
         empresa_obj = Empresa(nome, senha, area, periodo, salario,202401,202412,cargaHoraria,cnpj,Snome,Scontato,Scargo)
         print(f"\nCadastro realizado: {empresa_obj.get_info()}")
         lista.append(empresa_obj)
@@ -55,7 +86,7 @@ def logon(lista):
 
     for item in lista:
         if isinstance(item, Aluno) or isinstance(item, Empresa):
-            if item.get_nome() == usuario and item.get_senha() == senha:
+            if item.get_nome().strip() == usuario and item.get_senha().strip() == senha:
                 print(f"\nVocê está logado:\n{item.get_info()}")
                 return True
 
