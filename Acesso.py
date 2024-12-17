5585eb2416deb15cf4bf3e55cdd0ed1cb32267cc
Anny <annymaria8865@gmail.com>
2024-12-17 11:43:54 -0400

    Codigo final.py
    Corrigi erros em try-except,  e adicionei: while True e break, e o comando raise para definir possíveis exceções.

--- Acesso.py
+++ Acesso.py
@@ -19,17 +19,21 @@ Nome, Senha, Área, Período, Dias Disponíveis, anoEscolar, idade, contato:''')
         periodo = input('Período: ').strip()
         diasdisponiveis = input('Dias Disponíveis: ').strip()
         anoEscolar = input('Ano escolar: ').strip()
-        try:
-            idade = int(input('Idade: '))
-        except ValueError:
+        while True:
+            try:
+                idade = int(input('Idade: '))
+                break 
+            except ValueError:
+                    print ("Ocorreu um erro! Insira um número inteiro válido.")
+        while True:
+            try:
+                contato = int(input('Contato: '))
+                break
+            except ValueError:
                 print ("Ocorreu um erro! Insira um número inteiro válido.")
-        try:
-            contato = int(input('Contato: '))
-        except ValueError:
-            print ("Ocorreu um erro! Insira um número inteiro válido.")
-            return
-        finally: 
-            print ("O 'try except' acabou, tente novamente! ")
+            
+        #finally: 
+        #print ("O 'try except' acabou, tente novamente! ")
 
         aluno_obj = Aluno(nome, senha, area, periodo, diasdisponiveis,anoEscolar,idade,contato)
         print(f"\nCadastro realizado: {aluno_obj.get_info()}")
@@ -43,16 +47,32 @@ Nome, Senha, Área, Período, Dias Disponíveis, anoEscolar, idade, contato:''')
         senha = input('Senha: ').strip()
         area = input('Área: ').strip()
         periodo = input('Período: ').strip()
+        
+
+        while True:   
+            try:
+                salario = int(input('Salário oferecido: '))
+                if salario < 300:
+                    raise Exception ("O salário não pode ser inferior a 300 reais")
+                break 
+            except:
+                print("O salário não pode ser inferior a 300 reais")
+                
+        while True:
+            try:
+                cargaHoraria = int(input('Carga hóraria: ')) 
+                break
+            except ValueError: 
+                print ("Insira dígitos numéricos válidos")
+            finally: 
+                print("Para concluir o cadastro")
+         
+
         cnpj = input('CNPJ: ').strip()
         Snome = input('Supervisor nome: ').strip()
         Scontato = input('Supervisor contato: ').strip()
         Scargo = input('Supervisor carg: ').strip()
-        cargaHoraria = int(input('Carga hóraria: ')) #colocar exception
-        salario = int(input('Salário oferecido: '))
-        if salario < 300:
-            raise Exception ("O salário não pode ser inferior a 300 reais") #ta dando errado
-    
-    
+            
         empresa_obj = Empresa(nome, senha, area, periodo, salario,202401,202412,cargaHoraria,cnpj,Snome,Scontato,Scargo)
         print(f"\nCadastro realizado: {empresa_obj.get_info()}")
         lista.append(empresa_obj)
@@ -68,8 +88,9 @@ def logon(lista):
 
     for item in lista:
         if isinstance(item, Aluno) or isinstance(item, Empresa):
-            if item.get_nome() == usuario and item.get_senha() == senha:
+            if item.get_nome().strip() == usuario and item.get_senha().strip() == senha:
                 print(f"\nVocê está logado:\n{item.get_info()}")
                 return True
 
     print("\nUsuário ou senha incorretos.")
+
